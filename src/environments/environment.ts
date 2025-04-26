4f3a7566377cc58f625d42feb0384ecd7e0f0933
Reinan Rodrigues <168472677+RodriguesReinan@users.noreply.github.com>
2025-04-26 09:05:38 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,13 @@ export const environment = {
   production: false,
   cepAPI: 'https://viacep.com.br/ws/',
   firebase: {
-    apiKey: "AIzaSyBc2Wu6Lu3XdOOHQtcW4DSLvvZw3ZmcnLA",
-    authDomain: "sistema-para-imobiliaria.firebaseapp.com",
-    projectId: "sistema-para-imobiliaria",
-    storageBucket: "sistema-para-imobiliaria.firebasestorage.app",
-    messagingSenderId: "1089407053798",
-    appId: "1:1089407053798:web:1849db1653dc7ffb3e3027",
-    measurementId: "G-2EH1DNP7H7"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   },
   apiUrl: ''
 };
